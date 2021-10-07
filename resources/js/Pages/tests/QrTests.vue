e3764d46fca4c7bf485392de560057fc879fb6e0
Mahmood Durazi <38852127+Durazim@users.noreply.github.com>
2021-10-07 22:19:48 +0300

    test

--- resources/js/Pages/tests/QrTests.vue
+++ resources/js/Pages/tests/QrTests.vue
@@ -1,37 +1,78 @@
 <template>
-    <div><qrcode-stream></qrcode-stream>
+    <div>
+        <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+
+        <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
+            <div v-show="showScanConfirmation" class="scan-confirmation">
+                <img :src="$withBase('/checkmark.svg')" alt="Checkmark" width="128px" />
+            </div>
+        </qrcode-stream>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue';
-import { QrcodeStream } from 'vue-qrcode-reader/'
+<script>
 
-export default defineComponent({
-    name: 'QrStreamExample',
-    components: {
-        QrcodeStream,
+import { QrcodeStream } from 'qrcode-reader-vue3'
+export default {
 
-    },
-    setup() {
-        const state = reactive({
-            data: null
-        })
-        function onDecode(data) {
-            state.data = data
-        }
+    components: { QrcodeStream },
+
+    data () {
         return {
-            ...toRefs(state),
-            onDecode
+            camera: 'auto',
+            result: null,
+            showScanConfirmation: false
+        }
+    },
+
+    methods: {
+
+        async onInit (promise) {
+            try {
+                await promise
+            } catch (e) {
+                console.error(e)
+            } finally {
+                this.showScanConfirmation = this.camera === "off"
+            }
+        },
+
+        async onDecode (content) {
+            this.result = content
+
+            this.pause()
+            await this.timeout(500)
+            this.unpause()
+        },
+
+        unpause () {
+            this.camera = 'auto'
+        },
+
+        pause () {
+            this.camera = 'off'
+        },
+
+        timeout (ms) {
+            return new Promise(resolve => {
+                window.setTimeout(resolve, ms)
+            })
         }
     }
-});
+}
 </script>
 
 <style scoped>
-.stream {
-    max-height: 500px;
-    max-width: 500px;
-    margin: auto;
+.scan-confirmation {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+
+    background-color: rgba(255, 255, 255, .8);
+
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
 }
 </style>
